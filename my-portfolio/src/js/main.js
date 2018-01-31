96d1cd4ff8eba274e51fccee5d45bb1b27402de2
Unknown <zerayah@gmail.com>
2018-01-31 18:21:23 +0100

    remade dark part

--- my-portfolio/src/js/main.js
+++ my-portfolio/src/js/main.js
@@ -1,17 +1,30 @@
 
 
-$('.progress-bar').appear();
+// $('.progress-bar').appear();
 
-// function progress(barID, value) {
-//     $(barID).animate({'width': value});
-// };
+// // function progress(barID, value) {
+// //     $(barID).animate({'width': value});
+// // };
+
+// $('.progress-bar').on('appear', function(){
+//     var progressValue = $(this).data('progress');
+//     $(this).delay(500).animate({width: progressValue},1000)
+
+
+// });
+
+$(document).ready(
+    function(){
+        $('.progress-bar').appear();
+
 
 $('.progress-bar').on('appear', function(){
     var progressValue = $(this).data('progress');
     $(this).delay(500).animate({width: progressValue},1000)
 
+    }
 
-});
+)
 
 // function progress(barID, value) {
 //         $(barID).animate({'width': value}, 500);
